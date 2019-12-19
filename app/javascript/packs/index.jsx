88f0557e67831074ad55085b736adc325efe8d39
Ryan Chase <in.rchase@gmail.com>
2019-12-18 20:37:55 -0800

    run a check for login status on component mount

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -8,6 +8,7 @@ import {BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from '../components/Login';
 import Home from '../components/Home/Home';
 import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios'
 
 class App extends Component {
   constructor() {
@@ -19,12 +20,47 @@ class App extends Component {
     };
 
     this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
   }
 
   handleLogin(data) {
     this.setState({
       loggedInStatus: "LOGGED_IN",
       user: data
+    })
+  }
+
+  checkLoginStatus() {
+    axios.get("/logged_in", { withCredentials: true })
+      .then(res => {
+        // console.log("loggied in?", JSON.stringify(res))
+        if(res.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "LOGGED_IN",
+            user: res.data.user
+          });
+        }
+        else if(!res.data.logged_in && this.state.loggedInStatus === "LOGGED_IN") {
+          // Log them out if our DB says their session is no longer good
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN",
+            user: res.data.user
+          });
+        }
+      })
+      .catch(err => {
+        console.log("login error", err);
+      })
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
+
+  handleLogout() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {}
     })
   }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
 
   validates_presence_of :email
-  validate_uniqueness_of :email
+  validates_uniqueness_of :email
 end
