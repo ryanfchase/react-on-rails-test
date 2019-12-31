c7c2f5656b1fd6ae32a5874f037a0cc76ee2b223
Ryan Chase <in.rchase@gmail.com>
2019-12-31 08:49:04 -0800

    getting ready to add modals

--- app/javascript/components/Home/Table/TableBasic.jsx
+++ app/javascript/components/Home/Table/TableBasic.jsx
@@ -5,6 +5,18 @@ let cartKey = 0;
 
 class TableBasic extends Component {
 
+  aggregateCart = () => {
+    return this.props.cart.reduce((aggregates, episode) => {
+      if (episode.title in aggregates) {
+        aggregates[episode.title] = aggregates[episode.title] + 1;
+      }
+      else {
+        aggregates[episode.title] = 1;
+      }
+      return aggregates;
+    }, {});
+  }
+
   render() {
 
     const items = this.props.course_episodes.map( (episode) => {
@@ -16,23 +28,19 @@ class TableBasic extends Component {
       );
     });
 
-    const cartAggregates = this.props.cart.reduce((aggregates, episode) => {
-      if (episode.title in aggregates) {
-        aggregates[episode.title] = aggregates[episode.title] + 1;
-      }
-      else {
-        aggregates[episode.title] = 1;
-      }
-      return aggregates;
-    }, {});
+    const cartAggregates = this.aggregateCart();
 
-    const cartItems = Object.keys(cartAggregates).map(key => {
+    const keys = Object.keys(cartAggregates);
+
+    const cartItems = keys.length > 0 ? keys.map(key => {
       return (
         <div className="row px-4 py1" key={cartKey++}>
           {key} - ({cartAggregates[key]})
         </div>
       );
-    })
+    }) : (
+      <div className="row px-4 py1"> (No Items Selected)</div>
+    );
 
     return (
       <div className="row">
